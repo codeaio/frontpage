f4bee488628570c86bd03ca14d1ddbbff27a60ff
Kartik <kartikprajapati987@gmail.com>
2020-08-22 19:28:13 +0530

    add MUI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Main from './components/home/main';
 import Footer from './components/home/footer';
 import Docs from './components/docs/docs';
 import Wrapper  from "./components/home/wrapper";
+import Login from './components/user/login';
+import Signup from './components/user/signup';
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -12,8 +15,11 @@ function App() {
     <div className="App">
       <Header />
       <Switch>
-        <Route path="/home" component={Wrapper} exact/>
+        <Route path="/" component={Wrapper} exact/>
         <Route path="/docs" component={Docs} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+
       </Switch>
     </div>
   );
